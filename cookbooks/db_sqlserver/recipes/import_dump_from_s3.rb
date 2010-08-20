b4e66f7a8a75334891ad545e55946ecebe70116c
Alex <Alex@Alex-PC>
2010-08-20 11:51:10 +0100

    renaming

--- cookbooks/db_sqlserver/recipes/import_dump_from_s3.rb
+++ cookbooks/db_sqlserver/recipes/import_dump_from_s3.rb
@@ -46,15 +46,15 @@ else
   sql_dump=@node[:s3][:file]
 
   # unzip the dump file 
-  if (@node[:s3][:file] =~ /(.*)\.zip/)
+  if (@node[:s3][:file] =! /(.*)\.sql/)
     sql_dump=$1
-    Chef::Log.info("*** Unzipping database dump.")
-    powershell "Unzipping "+@node[:s3][:file] do
-      parameters({'ZIPPED_FILE' => @node[:s3][:file]})
+    Chef::Log.info("*** Unpacking database dump.")
+    powershell "Unpacking "+@node[:s3][:file] do
+      parameters({'PACKAGE' => @node[:s3][:file]})
       # Create the powershell script
       powershell_script = <<'POWERSHELL_SCRIPT'
         cd c:/tmp
-        cmd /c 7z x -y "c:/tmp/${env:ZIPPED_FILE}"
+        cmd /c 7z x -y "c:/tmp/${env:PACKAGE}"
 POWERSHELL_SCRIPT
       source(powershell_script)
     end
