e08a8b588a44561d3d9616c71fd93be470205366
Alex <Alex@Alex-PC>
2010-09-21 12:23:27 +0100

    ip

--- cookbooks/lb_haproxy/recipes/do_attach_request.rb
+++ cookbooks/lb_haproxy/recipes/do_attach_request.rb
@@ -9,12 +9,16 @@
 
 right_link_tag "loadbalancer:app=#{@node[:lb_haproxy][:applistener_name]}"
 
+Chef::Log.info("*** Private ip is: #{@node[:network][:interfaces][:configuration][:ip_address]}")
+#@node[:network][:interfaces][:configuration][:ip_address]
+
 remote_recipe "Attach me to load ballancer" do
   recipe "lb_haproxy::do_attach"
   attributes :remote_recipe => {
                 :backend_ip => "10.48.11.63",
                 :backend_id => @node[:rightscale][:instance_uuid]
               }
+              
   recipients_tags "loadbalancer:lb=#{@node[:lb_haproxy][:applistener_name]}"
 end
 
