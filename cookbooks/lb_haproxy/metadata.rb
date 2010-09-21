af88adde0267d4f05aa17123ddda675792f068fb
Alex <Alex@Alex-PC>
2010-09-21 11:18:13 +0100

    detach

--- cookbooks/lb_haproxy/metadata.rb
+++ cookbooks/lb_haproxy/metadata.rb
@@ -6,9 +6,10 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version "0.0.2"
 
 recipe "lb_haproxy::do_attach_request", "Attaches an application server to the load balancer"
+recipe "lb_haproxy::do_detach_request", "Detaches an application server from the load balancer"
 
 attribute "lb_haproxy/applistener_name",
   :display_name => "Applistener Name",
   :description => "Sets the name of the HAProxy load balance pool on frontends in /home/haproxy/rightscale_lb.cfg. Application severs will join this load balance pool by using this name. Ex: www",
-  :recipes => [ 'lb_haproxy::do_attach_request'],
+  :recipes => ['lb_haproxy::do_attach_request', 'lb_haproxy::do_deattach_request'],
   :required => "required"
